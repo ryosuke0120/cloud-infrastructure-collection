cde42729289560c5a33f02c0d0a9a9cb81d0e14c
Nishida Ryosuke <55966582+24daryo@users.noreply.github.com>
2024-06-17 00:25:14 +0900

    fix variables

--- case1-FastAPI-CloudRun/commands/step2-upload-github-secrets.sh
+++ case1-FastAPI-CloudRun/commands/step2-upload-github-secrets.sh
@@ -15,13 +15,13 @@ source .env
 # プロジェクトIDを設定
 GCP_PROJECT_ID="${GCP_PROJECT_ID}"
 GCP_REGION="${GCP_REGION}"
-GCP_SERVICE_ACCOUNT_NAME="${GCP_SERVICE_ACCOUNT_NAME}"
+GCP_SERVICE_NAME="${GCP_SERVICE_NAME}"
 GCP_SA_KEY=$(cat "service-account-key.json")
 
 # GitHub Secretsのアップロード
 gh secret set GCP_PROJECT_ID -b"${GCP_PROJECT_ID}" --repo "${REPO}"
 gh secret set GCP_REGION -b"${GCP_REGION}" --repo "${REPO}"
-gh secret set GCP_SERVICE_ACCOUNT_NAME -b"${GCP_SERVICE_ACCOUNT_NAME}" --repo "${REPO}"
+gh secret set GCP_SERVICE_NAME -b"${GCP_SERVICE_ACCOUNT_NAME}" --repo "${REPO}"
 gh secret set GCP_SA_KEY -b"${GCP_SA_KEY}" --repo "${REPO}"
 
 echo "GitHub Secrets have been uploaded."
